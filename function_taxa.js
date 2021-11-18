71fb6212e00829f3f1d537b83abac7116b158597
cabraldasilvac <wcabraldasilvac@gmail.com>
2021-11-18 12:09:58 -0300

    removi arquivos

--- function_taxa.js
+++
@@ -1,7 +0,0 @@
-// Calcula valor com taxa de maquininha
-var taxa = function() {
-    var rec = document.getElementById('rec').value
-    var tx = document.getElementById('tx').value
-    var resultado = parseFloat(rec) * parseFloat(tx);
-    document.querySelector('.resultado').innerHTML = resultado;
-}
